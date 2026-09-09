1b981cdd3baf6abc290dabbc83c02593f1b5013f
Lorran Rodrigues <lorrancode@gmail.com>
2026-09-08 21:51:04 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [arr, setArr] = useState([])
+  useEffect(() => {
+    const fetchPromise = fetch('https://fakestoreapi.com/products')
+              
+    fetchPromise.then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error`)
+      }
+      return response.json();
+    }).then((data) => {
+      setArr(data)
+    })
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Praticando Promises</h1>
+      {
+        arr.map(el => (
+          <div>
+            <li>{el.title}</li>
+            <b>{el.price}</b>
+          </div>
+        ))
+      }
     </div>
   );
 }
